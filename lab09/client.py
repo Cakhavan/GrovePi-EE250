9038d059b5d304d707dde5f6971ab620b2a56086
Cameron Akhavan <cameronakhavan@Camerons-MacBook-Pro-2.local>
2018-04-17 19:52:38 -0700

    finasl

--- lab09/client.py
+++ lab09/client.py
@@ -6,7 +6,7 @@ import math
 from grove_rgb_lcd import *
 
 
-# MQTT variables
+# MQTT variables and topics
 broker_hostname = "eclipse.usc.edu"
 broker_port = 11000
 led_topic = "anrg-pi12/led"
@@ -14,19 +14,21 @@ lcd_topic= "anrg-pi12/lcd"
 humidity_topic = "anrg-pi12/humidity"
 temp_topic = "anrg-pi12/temperature" 
 
+#set up GrovePi temp/hum sensor
 sensor = 7 
 blue = 0    # The Blue colored sensor.
 white = 1   # The White colored sensor.
 
-
+#initialize and set variables
 led = "0"
 temperature = 0
 hum = 0
 lcd = ""
 
-
+#set Pin as output for led
 pinMode(5,"OUTPUT")
 
+#Set lcd screen
 setRGB(0,255,0)
 
 
@@ -34,34 +36,35 @@ setRGB(0,255,0)
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
+
+    #subscribe to topics
     client.subscribe(led_topic)
     client.subscribe(lcd_topic)
+
+    #create callbacks
     client.message_callback_add(led_topic, led_callback)
     client.message_callback_add(lcd_topic,lcd_callback)
+
+
 
 def led_callback(client, userdata, msg):
     global led
     
-    # if str(msg.payload, "utf-8") == "1":
-    #     print("Yep")
-    #     if led == "1":
-    #         print("Turned off")
-    #         digitalWrite(3, 0)
-    #         led = "0"
-    #     else:
-    #         print("Turned on")
-    #         digitalWrite(3, 1)
-    #         led = "1"
+    #toggle the led variable when a message is received
     if led == "1" and str(msg.payload,"utf-8") == "1":
         
+        #turn off led
         digitalWrite(5,0)
+        #toggle back variable to 0
         led=  "0"
 
     else: 
+        #turn on led
         digitalWrite(5,1)
         led = str(msg.payload, "utf-8")
 
 
+    #print led status to terminal for testing
     print("on_message: " + msg.topic + " " + led)
     if led == "0":
             print("LED IS OFF")
@@ -71,14 +74,12 @@ def led_callback(client, userdata, msg):
 
 def lcd_callback(client, userdata, msg):
     global lcd
+    #convert msg to utf-8 string
     lcd = str(msg.payload, "utf-8")
+    #print to screen 
     setText(lcd)
+    #print to terminal for testing
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
-
-
-
-def on_message(client, userdata, msg): 
-    print(msg.topic + " " + str(msg.payload))
 
 
 if __name__ == '__main__':
@@ -94,9 +95,12 @@ if __name__ == '__main__':
     global blue
     while True:
 
+        #gather temp,hum data from sensor
         [temp,humidity] = grovepi.dht(sensor, blue) 
+        #print data to terminal for testing
         print("Temp: " + str(temp) + "\n")
         print("Humidity: " + str(humidity) + "\n")
+        #publish hum/temp to respective topics 
         client.publish(humidity_topic, humidity)
         client.publish(temp_topic, temp)
             
